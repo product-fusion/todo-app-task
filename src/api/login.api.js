50f9a1c97a153eeb3d2b1a7c449cf87c0d570909
Arun J <arunjegan001@gmail.com>
2024-01-30 23:20:53 +0530

    empty commit

--- src/api/login.api.js
+++ src/api/login.api.js
@@ -9,7 +9,7 @@ class Login {
             password,
             client_id: 'Wxm1OWz8Mrhu4gOSNvSyWhgoOR0Pa8DZ',
             client_secret: 'c243Fb5uTy_L6n3iTy6C85OBhm10nGSnuuluDnfL566xDJ_ay459kTZtNUDRz4N3',
-            audience: 'https://dev-kwqlzbkwaupuhibz.us.auth0.com/api/v2/',
+            audience: 'https://dev-kwqlzbkwaupuhibz.us.auth0.com/api/v2/', 
           });
       const output = response?.status;
       return { output };
